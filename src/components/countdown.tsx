11090ddb14b8da0c3fc9aa16ccbf61fe4ce573eb
Byteorem <bytetheorem@gmail.com>
2025-09-30 11:41:25 -0400

    fixed linting

--- src/components/countdown.tsx
+++ src/components/countdown.tsx
@@ -1,13 +1,26 @@
 import NumberFlow, { NumberFlowGroup } from "@number-flow/react";
+import { useEffect } from "react";
+import { useCountdown } from "usehooks-ts";
 
 type Props = {
 	seconds: number;
 };
 
 export default function Countdown({ seconds }: Props) {
-	const hh = Math.floor(seconds / 3600);
-	const mm = Math.floor((seconds % 3600) / 60);
-	const ss = seconds % 60;
+	const [count, { startCountdown, resetCountdown }] = useCountdown({
+		countStart: seconds,
+		intervalMs: 1000,
+		countStop: 0,
+	});
+
+	useEffect(() => {
+		resetCountdown();
+		startCountdown();
+	}, [resetCountdown, startCountdown]);
+
+	const hh = Math.floor(count / 3600);
+	const mm = Math.floor((count % 3600) / 60);
+	const ss = count % 60;
 
 	return (
 		<NumberFlowGroup>
@@ -20,20 +33,32 @@ export default function Countdown({ seconds }: Props) {
 				}
 				className="flex items-baseline font-semibold text-3xl"
 			>
+				{hh > 0 && (
+					<>
+						<NumberFlow
+							trend={-1}
+							value={hh}
+							format={{ minimumIntegerDigits: 2 }}
+						/>
+						<NumberFlow
+							prefix=":"
+							trend={-1}
+							value={mm}
+							digits={{ 1: { max: 5 } }}
+							format={{ minimumIntegerDigits: 2 }}
+						/>
+					</>
+				)}
+				{hh === 0 && mm > 0 && (
+					<NumberFlow
+						trend={-1}
+						value={mm}
+						digits={{ 1: { max: 5 } }}
+						format={{ minimumIntegerDigits: 2 }}
+					/>
+				)}
 				<NumberFlow
-					trend={-1}
-					value={hh}
-					format={{ minimumIntegerDigits: 2 }}
-				/>
-				<NumberFlow
-					prefix=":"
-					trend={-1}
-					value={mm}
-					digits={{ 1: { max: 5 } }}
-					format={{ minimumIntegerDigits: 2 }}
-				/>
-				<NumberFlow
-					prefix=":"
+					prefix={hh > 0 || mm > 0 ? ":" : ""}
 					trend={-1}
 					value={ss}
 					digits={{ 1: { max: 5 } }}
